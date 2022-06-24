4996e55ea3694d77a3cd4dab1941e3ecb7b8514d
khantuevao <khantuevao@gmail.com>
2022-06-24 17:49:30 +0200

    Create logic to add new projects to array and dom

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import addProject from './add-project';
+import { projectsArray } from './projects-array';
+
+
+const consoleBtn = document.getElementById('consoleBtn');
+consoleBtn.addEventListener('click', () => {
+  console.log(projectsArray);
+});
+
+
+
+
+
